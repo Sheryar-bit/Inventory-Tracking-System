cb0f25c985f031ee93fc3509eb95525d4fe36a32
Sheryar <alisharyar93@gmail.com>
2025-04-06 00:07:38 +0500

    Redis Connection (get,set,del) cache

---
+++ middleware/Cache.js
@@ -0,0 +1,50 @@
+const redis = require('redis')
+
+const redisClient = redis.createClient();
+
+// Redis Connection
+(
+    async function() {
+        redisClient.on('error', function(err) {
+            console.error('Redis error:', err);
+        })
+
+        redisClient.on('ready', function () {
+            console.log("Redis Client Connected! ");
+            
+        })
+
+        await redisClient.connect();
+        await redisClient.ping();
+    }
+)()
+
+//Setting data into cache:
+const setCache = async function (key, data, expiry = 300) {
+    const value = typeof data === 'string' ? data : JSON.stringify(data);
+    await redisClient.set(key, value, { EX: expiry });
+}
+
+//Geting data from redis
+const getCache = async function (key) {
+    const value = await redisClient.get(key);
+    if (!value) return null;
+
+    try {
+        return JSON.parse(value); 
+    } catch (e) {
+        return value
+    }
+}
+
+
+const deleteCache = async function (key) {
+    await redisClient.del(key);
+}
+
+
+module.exports = {
+    setCache,
+    getCache,
+    deleteCache
+};
