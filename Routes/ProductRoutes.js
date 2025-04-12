b03a6c628bd19759e37a75d08b2338028f99c977
Sheryar <alisharyar93@gmail.com>
2025-04-12 12:24:46 +0500

    added Auth

--- Routes/ProductRoutes.js
+++ Routes/ProductRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { createProduct, getAllProduct, UpdateProduct, deleteProduct, getProductInventory } = require('../controllers/ProductController')
+const authenticate = require('../middleware/Auth')
 const app = express();
 
 const router = express.Router()
@@ -8,8 +9,8 @@ const router = express.Router()
 router.post('/products', createProduct);
 router.get('/products/:productId/inventory', getProductInventory);
 router.get('/products', getAllProduct);
-router.patch('/products/:productId', UpdateProduct);
-router.delete('/product/:productId', deleteProduct);
+router.patch('/products/:productId', authenticate, UpdateProduct);
+router.delete('/product/:productId', authenticate,deleteProduct);
 
 
 module.exports=router 
