4a36ff3eb20bcbbfb07ecf931711c1384a364efc
Sheryar <alisharyar93@gmail.com>
2025-04-12 13:08:14 +0500

    added authentication

--- Routes/StoreRoutes.js
+++ Routes/StoreRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { addstore, viewstores, getsinglestore, getStoreStock, updatestore, deletestore } = require('../controllers/StoreController')
+const authenticate = require('../middleware/Auth'); 
 const app = express();
 
 
@@ -9,8 +10,8 @@ router.post('/store', addstore);
 router.get('/stores', viewstores);
 router.get('/stores/:storeId', getsinglestore);
 router.get('/stores/:storeId/stock', getStoreStock);
-router.patch('/stores/:storeId', updatestore);
-router.delete('/store/:storeId', deletestore)
+router.patch('/stores/:storeId', authenticate ,updatestore);
+router.delete('/store/:storeId', authenticate,deletestore)
 
 
 
